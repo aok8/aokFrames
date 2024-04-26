3039a911a8410bf413dd062380c06dbc59794a0c
Alain Kouassi <aokouassi@gmail.com>
2024-04-26 16:12:30 -0700

    Finalized splashLogo timing and renamed incorrectly named path

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -23,169 +23,146 @@ const SplashScreen = ({ finishLoading }: { finishLoading: () => void }) => {
       easing: 'easeInOutSine',
       targets: '#logo #a11',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 500 // GSAP's true for drawSVG is equivalent to full line being drawn
-    }, 0) // Offset at 0 to chain animations
+      duration: 200
+    }, '-=50') // Offset at 0 to chain animations
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #a12',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 1000
-    }, 0)
+      duration: 300
+    }, '-=50')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #a13',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 1500
-    }, 0)
+      duration: 300
+    }, '-=50')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #a2',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 2000
-    }, 0)
+      duration: 100
+    }, '-=50')
     .add({
       easing: 'easeInOutSine',
-      targets: '#logo #a3',
+      targets: '#logo #o',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 2500
-    }, 0)
+      duration: 700
+    }, '+=50')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #k11',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 3000
-    }, 0)
+      duration: 200
+    }, '+=50')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #k12',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 3500
-    }, 0)
+      duration: 200
+    }, '-=100')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #k13',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 4000
-    }, 0)
+      duration: 200
+    }, '-=100')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #k2',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 4500
-    }, 0)
+      duration: 300
+    }, '-=50')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #k3',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 5000
-    }, 0)
+      duration: 300
+    }, '-=50')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #f1',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 5500
-    }, 0)
+      duration: 200
+    }, '+=50')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #f21',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 6000
-    }, 0)
+      duration: 200
+    }, '-=100')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #f22',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 6500
-    }, 0)
+      duration: 200
+    }, '-=100')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #f3',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 7000
-    }, 0)
+      duration: 300
+    }, '-=50')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #rames1',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 7500
-    }, 0)
+      duration: 400
+    }, '+=50')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #rames2',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 8000
-    }, 0)
+      duration: 500
+    }, '-=50')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #rames3',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 8500
-    }, 0)
+      duration: 300
+    }, '-=50')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #rames4',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 9000
-    }, 0)
+      duration: 300
+    }, '-=50')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #rames5',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 9500
-    }, 0)
+      duration: 300
+    }, '-=50')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #rames6',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 10000
-    }, 0)
+      duration: 500
+    }, '-=50')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #rames7',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 10500
-    }, 0)
+      duration: 400
+    }, '-=50')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #rames8',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 11000
-    }, 0)
+      duration: 300
+    }, '-=50')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo #rames9',
       strokeDashoffset: [anime.setDashoffset, 0],
-      duration: 700,
-      delay: 11500
-    }, 0)
+      duration: 300
+    }, '-=50')
     .add({
       easing: 'easeInOutSine',
       targets: '#logo',
       opacity: 0,
-      duration: 300
-    })
+      duration: 1000
+    },'+=50')
     .add({
       targets: '#splashScreen',
       backgroundColor: '#f1efe9', // Change to black
